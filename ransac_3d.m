b2a0936478be0e2fc7cfa2f826674bf87e6704f6
Christian Pfitzner <christian.pfitzner@evocortex.com>
2023-04-26 20:17:03 +0200

    Removed solution.

--- ransac_3d.m
+++ ransac_3d.m
@@ -9,6 +9,7 @@ pkg load statistics
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % generate noisy data
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 P = generate_noisy_plane(100,10); 
 
 
@@ -16,8 +17,10 @@ num = length(P(1,:));
 
 
 
+
+
 best_consens_size     = 0;
-best_plane_parameters = zeros(4,0);
+best_plane_parameters = ones(4,0);
 for trial=1:20
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -30,54 +33,37 @@ for trial=1:20
     rnd_b = randi(num);
     rnd_c = randi(num);
 
-    A = P(:, rnd_a);
-    C = P(:, rnd_c);
-    B = P(:, rnd_b);
-        
-    n = cross((B - A),(C - A));
-    n = n/norm(n);
-    d = n(1)*A(1) + n(2)*A(2) + n(3)*A(3);
+
+
+
+
 
     
     % check how many points belong to the consensus set
-    consensus_size = 0; 
-    for i=1:num
-        R    = P(:, i);
-        dist = n(1)*R(1) + n(2)*R(2) + n(3)*R(3) - d;
-        
-        if(abs(dist) < 0.2)
-        consensus_size = consensus_size + 1;
-        end
-        
-    end
-    
-    % in case the a better solution is found
-    if(consensus_size > best_consens_size)
-        best_consens_size     = consensus_size 
-        best_plane_parameters = [n; d]; 
-        trial
-    end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % YOUR CODE HERE
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+    % in case the a better solution is found, save the best plane parameters
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % YOUR CODE HERE
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % YOUR CODE HERE
 % Impement Least Square
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-a_11 = sum(P(1,:).^2);
-a_12 = sum(P(1,:).*P(2,:));
-a_13 = sum(P(1,:));
 
-a_21 = a_12; 
-a_22 = sum(P(2,:).^2);
-a_23 = sum(P(2,:));
 
-a_31 = a_13; 
-a_32 = a_23; 
-a_33 = length(P(1,:));
 
-b_1 = sum(P(1,:).*P(3,:));
-b_2 = sum(P(2,:).*P(3,:));
-b_3 = sum(P(3,:));
+
+
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -86,19 +72,12 @@ b_3 = sum(P(3,:));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-##mat_A = [a_11, a_12, a_13; 
-##     a_21, a_22, a_23; 
-##     a_31, a_32, a_33]
-##     
-##     
-##B = [b_1; b_2; b_3]
-##
-##
-##X = inv(A)*B
-##
-##
-##
-##
+
+
+
+
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % plot the result
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -107,21 +86,21 @@ scatter3(P(1,:), P(2,:), P(3,:));
 xlabel('x axis');
 ylabel('y axis');
 zlabel('z axis');
-xlim([-0.2; 1.2]);
-ylim([-0.2; 1.2]);
-zlim([-0.2; 3]);
+##xlim([-0.2; 1.2]);
+##ylim([-0.2; 1.2]);
+##zlim([-0.2; 3]);
 axis equal; 
-hold on; 
-tx = linspace (0, 1, 10);
-ty = linspace (0, 1, 10)';
+% hold on; 
+% tx = linspace (0, 1, 10);
+% ty = linspace (0, 1, 10)';
 
-a = best_plane_parameters(1); 
-b = best_plane_parameters(2); 
-c = best_plane_parameters(3); 
-d = best_plane_parameters(4); 
+% a = best_plane_parameters(1); 
+% b = best_plane_parameters(2); 
+% c = best_plane_parameters(3); 
+% d = best_plane_parameters(4); 
 
-tz = -(a.*tx + b.*ty - d) ./ c; % + 0.1;
-mesh(tx, ty, tz);
+% tz = -(a.*tx + b.*ty - d) ./ c; % + 0.1;
+% mesh(tx, ty, tz);
 
 
 
